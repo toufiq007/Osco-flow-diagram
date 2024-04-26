2a8b00a30d7135b37cc421d8c037ae797087da25
toufiq007 <t.limon2342@gmail.com>
2024-04-26 09:02:33 +0600

    make our node dragable

--- src/components/flow/Flow.tsx
+++ src/components/flow/Flow.tsx
@@ -1,8 +1,16 @@
-import ReactFlow, { Background, Controls } from "reactflow";
+import { useCallback, useState } from "react";
+import ReactFlow, {
+  Background,
+  Controls,
+  OnEdgesChange,
+  OnNodesChange,
+  applyEdgeChanges,
+  applyNodeChanges,
+} from "reactflow";
 import { Node, Edge } from "reactflow";
 import "reactflow/dist/style.css";
 // creating node
-const nodes: Node[] = [
+const initialNodes: Node[] = [
   {
     id: "1",
     position: { x: 0, y: 0 },
@@ -26,17 +34,33 @@ const nodes: Node[] = [
   },
 ];
 // adding edge
-const edges: Edge[] = [
-  { id: "1-2", source: "1", target: "2", animated: true },
+const initialEdes: Edge[] = [
+  { id: "1-2", source: "1", target: "2", animated:true },
   { id: "2-3", source: "2", target: "3", animated: true },
   { id: "3-4", source: "3", target: "4", animated: true },
 ];
 
 // our main diagram
 const FlowDiagram = () => {
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdes);
+
+  const onNodesChange: OnNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    []
+  );
+  const onEdgesChange: OnEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    []
+  );
   return (
     <div style={{ width: "100%", height: "90vh" }}>
-      <ReactFlow nodes={nodes} edges={edges}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+      >
         <Background />
         <Controls />
       </ReactFlow>
