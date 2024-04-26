b2ea0790d46a774dcc485d9ae83a1f710ae49a03
toufiq007 <t.limon2342@gmail.com>
2024-04-26 09:19:31 +0600

    connecting nodes with another node with adding edge

--- src/components/flow/Flow.tsx
+++ src/components/flow/Flow.tsx
@@ -6,6 +6,8 @@ import ReactFlow, {
   OnNodesChange,
   applyEdgeChanges,
   applyNodeChanges,
+  addEdge,
+  OnConnect,
 } from "reactflow";
 import { Node, Edge } from "reactflow";
 import "reactflow/dist/style.css";
@@ -34,25 +36,39 @@ const initialNodes: Node[] = [
   },
 ];
 // adding edge
-const initialEdes: Edge[] = [
-  { id: "1-2", source: "1", target: "2", animated:true },
-  { id: "2-3", source: "2", target: "3", animated: true },
-  { id: "3-4", source: "3", target: "4", animated: true },
-];
+// const initialEdes: Edge[] = [
+//   { id: "1-2", source: "1", target: "2", animated: true },
+//   { id: "2-3", source: "2", target: "3", animated: true },
+//   { id: "3-4", source: "3", target: "4", animated: true, label: "to the" },
+// ];
+const initialEdes: Edge[] = [];
 
 // our main diagram
 const FlowDiagram = () => {
   const [nodes, setNodes] = useState<Node[]>(initialNodes);
   const [edges, setEdges] = useState<Edge[]>(initialEdes);
 
+  // handle node changes
   const onNodesChange: OnNodesChange = useCallback(
     (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
     []
   );
+  // handle edge changes
   const onEdgesChange: OnEdgesChange = useCallback(
     (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
     []
   );
+
+  // adding new edge
+  // const onConnect: OnConnect = useCallback(
+  //   (params) => setEdges((eds) => addEdge(params, eds)),
+  //   []
+  // );
+
+  const onConnect: OnConnect = useCallback((params) => {
+    setEdges((eds) => addEdge(params, eds));
+  }, []);
+
   return (
     <div style={{ width: "100%", height: "90vh" }}>
       <ReactFlow
@@ -60,6 +76,7 @@ const FlowDiagram = () => {
         edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
       >
         <Background />
         <Controls />
