fb4e5e2e37308c11a167e87f4d2ef5d883946bb6
toufiq007 <t.limon2342@gmail.com>
2024-04-26 08:54:04 +0600

    created our very first node and edge in flow

--- src/components/flow/Flow.tsx
+++ src/components/flow/Flow.tsx
@@ -1,16 +1,42 @@
 import ReactFlow, { Background, Controls } from "reactflow";
+import { Node, Edge } from "reactflow";
 import "reactflow/dist/style.css";
-const nodes = [
+// creating node
+const nodes: Node[] = [
   {
     id: "1",
-    position: { x: 50, y: 350 },
-    data: { label: "Node 1" },
+    position: { x: 0, y: 0 },
+    data: { label: "node 1" },
+  },
+  {
+    id: "2",
+    position: { x: 500, y: 100 },
+    data: { label: "node 2" },
+  },
+
+  {
+    id: "3",
+    position: { x: 300, y: 600 },
+    data: { label: "node 3" },
+  },
+  {
+    id: "4",
+    position: { x: 700, y: 300 },
+    data: { label: "node 4" },
   },
 ];
+// adding edge
+const edges: Edge[] = [
+  { id: "1-2", source: "1", target: "2", animated: true },
+  { id: "2-3", source: "2", target: "3", animated: true },
+  { id: "3-4", source: "3", target: "4", animated: true },
+];
+
+// our main diagram
 const FlowDiagram = () => {
   return (
     <div style={{ width: "100%", height: "90vh" }}>
-      <ReactFlow nodes={nodes}>
+      <ReactFlow nodes={nodes} edges={edges}>
         <Background />
         <Controls />
       </ReactFlow>
