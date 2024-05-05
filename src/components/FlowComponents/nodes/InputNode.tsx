708c5c6b3da1b7e197627b2b3a464477db902f94
toufiq007 <t.limon2342@gmail.com>
2024-05-05 21:02:32 +0600

    install react icons to the project. add dynamic input when click add new input. in custom inputNode

--- src/components/FlowComponents/nodes/InputNode.tsx
+++ src/components/FlowComponents/nodes/InputNode.tsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import { Handle, Position } from "reactflow";
+import { RemoveCircle } from "@mui/icons-material";
 
 const InputNode = () => {
   const [changes, setChanges] = useState("");
-  const handleChanges = (changes: string) => {
-    console.log(changes);
+  console.log(changes);
+
+  // create dynamic input
+  const initialInput = [{ id: 1, value: "" }];
+  const [inputs, setInputs] = useState(initialInput);
+  const addNewInput = () => {
+    setInputs([...inputs, { id: inputs.length + 1, value: "" }]);
+  };
+  const removeInput = (id: number) => {
+    const newInputs = inputs.filter((input) => input.id !== id);
+    setInputs(newInputs);
   };
   return (
     <div>
@@ -15,16 +25,27 @@ const InputNode = () => {
           display: "flex",
           flexDirection: "column",
           padding: "1rem",
+          borderRadius: "20px",
         }}
       >
         <label htmlFor="text">Text:</label>
-        <input
-          id="text"
-          name="text"
-          onChange={(e) => setChanges(e.target.value)}
-          className="nodrag"
-          style={{ border: "1px solid #000" }}
-        />
+        {inputs.length > 0 &&
+          inputs.map((input) => {
+            return (
+              <div key={input.id} style={{ marginTop: "10px" }}>
+                <input
+                  id="text"
+                  name="text"
+                  onChange={(e) => setChanges(e.target.value)}
+                  className="nodrag"
+                  style={{ border: "1px solid #000", marginRight: "5px" }}
+                />
+                <button onClick={() => removeInput(input.id)}>
+                  <RemoveCircle />
+                </button>
+              </div>
+            );
+          })}
         <button
           style={{
             marginTop: ".5rem",
@@ -33,9 +54,9 @@ const InputNode = () => {
             color: "#000",
             cursor: "pointer",
           }}
-          onClick={() => handleChanges(changes)}
+          onClick={addNewInput}
         >
-          Create node
+          Create new input
         </button>
       </div>
       <Handle type="source" position={Position.Bottom} id="c" />
